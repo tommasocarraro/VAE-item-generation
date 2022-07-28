bug fix in metric computation

main_mf.py
@@ -8,10 +8,10 @@ from vaeitemgen.nets import MatrixFactorization
 # get data
 d = AmazonFashion("./dataset/AmazonFashion6ImgPartitioned.npy", with_images=False)
 # create network architecture
-v = MatrixFactorization(d.n_users, d.n_items, 10).to(vaeitemgen.device)
+v = MatrixFactorization(d.n_users, d.n_items, 100).to(vaeitemgen.device)
 # create loaders for training and validation
-tr_loader = DataLoader(d.folds["train"], d.u_i_matrix, 64)
-val_loader = DataLoader(d.folds["val"], d.u_i_matrix, 64, shuffle=False)
+tr_loader = DataLoader(d.folds["train"], d.u_i_matrix, 128)
+val_loader = DataLoader(d.folds["val"], d.u_i_matrix, 128, n_neg=100, shuffle=False)
 # training the model
 optimizer = torch.optim.Adam(v.parameters(), lr=0.001, weight_decay=0.0001)
 model = TrainerMF(v, optimizer)

vaeitemgen/loaders.py
@@ -84,17 +84,21 @@ class DataLoader:
                  data,
                  u_i_matrix,
                  batch_size=1,
+                 n_neg=1,
                  shuffle=True):
         """
         Constructor of the training data loader.
         :param data: np.array of triples (user, item, rating)
         :param u_i_matrix: sparse user-item interaction matrix where there is a 1 if user i interacted with item j
         :param batch_size: batch size for the training of the model
+        :param n_neg: number of negative items that have to be sampled and given with the positive item. 1 during
+        training and 100 during validation and test
         :param shuffle: whether to shuffle data during training or not
         """
         self.data = data
         self.u_i_matrix = u_i_matrix
         self.batch_size = batch_size
+        self.n_neg = n_neg
         self.shuffle = shuffle
 
     def __len__(self):
@@ -115,8 +119,8 @@ class DataLoader:
             # get negative item indexes
 
             user_interactions = self.u_i_matrix[u_idx]
-            n_i_idx = np.stack([np.random.permutation((1 - user_interactions[u].todense()).nonzero()[1])[0]
-                       for u in range(user_interactions.shape[0])])
+            n_i_idx = np.stack([np.random.permutation((1 - user_interactions[u].todense()).nonzero()[1])[:self.n_neg]
+                                for u in range(user_interactions.shape[0])])
 
             yield torch.tensor(u_idx).to(vaeitemgen.device), \
-                  torch.tensor(np.stack([p_i_idx, n_i_idx], axis=1)).to(vaeitemgen.device)
+                  torch.tensor(np.concatenate([p_i_idx[:, np.newaxis], n_i_idx], axis=1)).to(vaeitemgen.device)

vaeitemgen/metrics.py
@@ -6,7 +6,8 @@ def auc(pos_pred, neg_pred):
     It computes the AUC measure as stated in the fashion recommendation paper.
 
     :param pos_pred: np.array of predictions for relevant items
-    :param neg_pred: np.array of predictions for non-relevant items
+    :param neg_pred: np.array of predictions for non-relevant items. During validation we have 100 negative predictions
+    for each positive prediction
     :return: the AUC measure of the given predictions
     """
-    return np.count_nonzero(pos_pred > neg_pred) / pos_pred.shape[0]
+    return np.mean(np.mean(pos_pred[:, np.newaxis] > neg_pred, axis=1))

vaeitemgen/models.py
@@ -197,15 +197,15 @@ class TrainerMF:
             # training step
             train_loss = self.train_epoch(train_loader, epoch + 1)
             # validation step
-            auc_score, val_loss = self.validate(val_loader)
+            auc_score = self.validate(val_loader)
             # print epoch data
             if (epoch + 1) % verbose == 0:
                 print("Epoch %d - Train loss %.3f - Validation AUC %.3f"
                       % (epoch + 1, train_loss, auc_score))
             # save best model and update early stop counter, if necessary
-            val_score = val_loss
-            if val_score > best_val_score:
-                best_val_score = val_score
+
+            if auc_score > best_val_score:
+                best_val_score = auc_score
                 early_counter = 0
                 if save_path:
                     self.save_model(save_path)
@@ -229,7 +229,7 @@ class TrainerMF:
                 self.optimizer.zero_grad()
                 rating_pred = self.model(u_idx, i_idx[:, 0])
                 n_rating_pred = self.model(u_idx, i_idx[:, 1])
-                loss = - torch.sum(torch.log(torch.nn.Sigmoid()(rating_pred - n_rating_pred)))
+                loss = - torch.mean(torch.log(torch.nn.Sigmoid()(rating_pred - n_rating_pred)))
                 loss.backward()
                 self.optimizer.step()
                 train_loss += loss.item()
@@ -259,14 +259,10 @@ class TrainerMF:
         auc_score, val_loss = [], 0.0
         for batch_idx, (u_idx, i_idx) in enumerate(val_loader):
             predicted_scores = self.predict(u_idx, i_idx[:, 0])
-            n_predicted_scores = self.predict(u_idx, i_idx[:, 1])
+            n_predicted_scores = self.predict(u_idx.unsqueeze(1).expand(-1, i_idx.shape[1] - 1), i_idx[:, 1:])
             auc_score.append(auc(predicted_scores.cpu().numpy(), n_predicted_scores.cpu().numpy()))
 
-            # compute validation loss
-
-            val_loss += - torch.mean(torch.log(torch.nn.Sigmoid()(predicted_scores - n_predicted_scores)))
-
-        return np.mean(auc_score), val_loss / len(val_loader)
+        return np.mean(auc_score)
 
     def save_model(self, path):
         """